45aae6be7dfffc2f41da2a03ad0989e6ce3fa654
Kamesuta <kamesuta@gmail.com>
2016-01-29 22:10:33 +0900

    Implement Chat

--- src/main/java/com/bebehp/mc/eewreciever/EEWRecieverMod.java
+++ src/main/java/com/bebehp/mc/eewreciever/EEWRecieverMod.java
@@ -1,11 +1,20 @@
 package com.bebehp.mc.eewreciever;
 
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.relauncher.Side;
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ServerChatEvent;
 
 @Mod(modid="EEWReciever", name="EEWReciever", version="1.0")
 public class EEWRecieverMod
@@ -16,5 +25,27 @@ public class EEWRecieverMod
 	public void init(FMLInitializationEvent event)
 	{
 		logger.info("EEW is setting up.");
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	public void onServerChat(ServerChatEvent event)
+	{
+		if (event.message.contains("fuck"))
+		{
+			EEWRecieverMod.sendServerChat("fuck you too");
+		}
+	}
+
+	@NetworkCheckHandler
+	public boolean netCheckHandler(Map<String, String> mods, Side side)
+	{
+		return true;
+	}
+
+	public static void sendServerChat(String msg)
+	{
+		FMLCommonHandler.instance().getMinecraftServerInstance()
+			.getConfigurationManager().sendChatMsg(new ChatComponentText(msg));
 	}
 }
---
+++ src/main/java/com/bebehp/mc/eewreciever/ping/WatchEEW.java
@@ -0,0 +1,8 @@
+package com.bebehp.mc.eewreciever.ping;
+
+/**
+ * 地震情報を確認します
+ * @author b7n
+ */
+public class WatchEEW {
+}
