794db876a905e9cd9a8021c10524472129b407bf
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-02-12 08:59:04 +0900

    fix

--- src/main/java/com/bebehp/mc/eewreciever/twitter/GetTweet.java
+++ src/main/java/com/bebehp/mc/eewreciever/twitter/GetTweet.java
@@ -18,11 +18,11 @@ public class GetTweet {
 	public void onStatus(Status status) {
 
 		 Configuration configuration = new ConfigurationBuilder()
-				 	.setOAuthConsumerKey(mh5mOJhrXkVarLLdNgDn2QFRO)
-				 	.setOAuthConsumerSecret(NbBfZ5ytY47IniUEOoFOIk0wqfOuByzqMzK26DqvH9GhVL0K3E)
-				 	setOAuthAccessToken(4893957312-30hXziVjdX0ZHzH6OJCv0eWAJmaDgyqR7Wwfjob)
-				 	.setOAuthAccessTokenSecret(ZwqJSMxSFC7lCMmAjgDw3ikwfgnJE9RVyTZt67MYIsMOM)
-				 	.build();
+				 .setOAuthConsumerKey(mh5mOJhrXkVarLLdNgDn2QFRO)
+				 .setOAuthConsumerSecret(NbBfZ5ytY47IniUEOoFOIk0wqfOuByzqMzK26DqvH9GhVL0K3E)
+				 .setOAuthAccessToken(4893957312-30hXziVjdX0ZHzH6OJCv0eWAJmaDgyqR7Wwfjob)
+				 .setOAuthAccessTokenSecret(ZwqJSMxSFC7lCMmAjgDw3ikwfgnJE9RVyTZt67MYIsMOM)
+				 .build();
 
 			TwitterStream twitterStream = new TwitterStreamFactory(configuration).getInstance();
 			StatusListener listener = new StatusListener() {
@@ -62,7 +62,7 @@ public class GetTweet {
 
 		// フィルター
 		FilterQuery filterQuery = new FilterQuery();
-		filterQuery.track(new String[] {"きんようび"});
+		filterQuery.track(new String[] {"from:eewbot"});
 		twitterStream.filter(filterQuery);
 	 }
 }
