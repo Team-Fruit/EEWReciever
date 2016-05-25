42ca3cb0028e6ecd9bc6ab01a614be8c8d147423
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-05-26 05:49:25 +0900

    Commit

--- src/main/java/com/bebehp/mc/eewreciever/twitter/TweetQuakeLocation.java
+++ src/main/java/com/bebehp/mc/eewreciever/twitter/TweetQuakeLocation.java
@@ -3,9 +3,9 @@ package com.bebehp.mc.eewreciever.twitter;
 import com.bebehp.mc.eewreciever.ping.MyNumber;
 import com.bebehp.mc.eewreciever.ping.QuakeLocation;
 
-//@Deprecated
+@Deprecated
 public class TweetQuakeLocation extends QuakeLocation {
-	public TweetQuakeLocation(String ns, String we) {
+	public TweetQuakeLocation(final String ns, final String we) {
 		super(new MyNumber(ns), new MyNumber(we));
 	}
 }
