9b32b202d1be884451df2dd7a6de3585ab8ac8a4
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-06-15 16:37:42 +0900

    Update 2.4 MC1.7.10

--- src/main/java/com/bebehp/mc/eewreciever/twitter/TweetQuake.java
+++ src/main/java/com/bebehp/mc/eewreciever/twitter/TweetQuake.java
@@ -10,6 +10,7 @@ import com.bebehp.mc.eewreciever.ping.AbstractQuakeNode;
 import com.bebehp.mc.eewreciever.ping.IQuake;
 import com.bebehp.mc.eewreciever.ping.QuakeException;
 
+import twitter4j.FilterQuery;
 import twitter4j.Status;
 import twitter4j.StatusAdapter;
 import twitter4j.StatusListener;
@@ -19,42 +20,48 @@ import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class TweetQuake implements IQuake {
-	private List<AbstractQuakeNode> updatequeue = new LinkedList<AbstractQuakeNode>();
-	private Configuration configuration;
-	private TwitterStream twitterStream;
-	private StatusListener listener;
+	private final List<AbstractQuakeNode> updatequeue = new LinkedList<AbstractQuakeNode>();
+	private final Configuration configuration;
+	private final TwitterStream twitterStream;
+	private final StatusListener listener;
 
 	public TweetQuake() {
-		configuration = new ConfigurationBuilder().setOAuthConsumerKey("mh5mOJhrXkVarLLdNgDn2QFRO")
+		this.configuration = new ConfigurationBuilder().setOAuthConsumerKey("mh5mOJhrXkVarLLdNgDn2QFRO")
 				.setOAuthConsumerSecret("NbBfZ5ytY47IniUEOoFOIk0wqfOuByzqMzK26DqvH9GhVL0K3E")
 				.setOAuthAccessToken("4893957312-30hXziVjdX0ZHzH6OJCv0eWAJmaDgyqR7Wwfjob")
 				.setOAuthAccessTokenSecret("ZwqJSMxSFC7lCMmAjgDw3ikwfgnJE9RVyTZt67MYIsMOM").build();
-		twitterStream = new TwitterStreamFactory(configuration).getInstance();
-		listener = new StatusAdapter() {
+		this.twitterStream = new TwitterStreamFactory(this.configuration).getInstance();
+		this.listener = new StatusAdapter() {
 			@Override
-			public void onStatus(Status status) {
+			public void onStatus(final Status status) {
 				try {
-					String str = new String(status.getText().getBytes("UTF-8"), "UTF-8").intern();
-					updatequeue.add(new TweetQuakeNode().parseString(str));
+					final String str = new String(status.getText().getBytes("UTF-8"), "UTF-8").intern();
+					TweetQuake.this.updatequeue.add(new TweetQuakeNode().parseString(str));
 					EEWRecieverMod.logger.info(status.getText());
-				} catch (UnsupportedEncodingException e) {
+				} catch (final UnsupportedEncodingException e) {
 					EEWRecieverMod.logger.error("Encode Error", e);
-				} catch (QuakeException e) {
+				} catch (final QuakeException e) {
 					EEWRecieverMod.logger.error("Recieve Error", e);
 				}
 			}
 		};
-		twitterStream.addListener(listener);
-		if(ConfigurationHandler.twitterEnable) twitterStream.user();
+		this.twitterStream.addListener(this.listener);
+		if (ConfigurationHandler.debugMode && ConfigurationHandler.twitterEnable)
+			this.twitterStream.user();
+		else if (ConfigurationHandler.twitterEnable) {
+			final FilterQuery query = new FilterQuery();
+			query.track(new String[] { "from:eewbot" });
+			this.twitterStream.filter(query);
+		}
 	}
 
 	@Override
 	public List<AbstractQuakeNode> getQuakeUpdate() throws QuakeException {
-		if (updatequeue.isEmpty())
-			return updatequeue;
+		if (this.updatequeue.isEmpty())
+			return this.updatequeue;
 
-		List<AbstractQuakeNode> ret = new LinkedList<AbstractQuakeNode>(updatequeue);
-		updatequeue.clear();
+		final List<AbstractQuakeNode> ret = new LinkedList<AbstractQuakeNode>(this.updatequeue);
+		this.updatequeue.clear();
 		return ret;
 	}
 }
