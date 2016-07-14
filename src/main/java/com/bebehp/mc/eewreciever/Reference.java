20d177996b42069fcbf971d5080384f7c683063d
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-07-14 17:25:50 +0900

    Commit

--- src/main/java/com/bebehp/mc/eewreciever/Reference.java
+++ src/main/java/com/bebehp/mc/eewreciever/Reference.java
@@ -6,9 +6,9 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
 	public static final String MODID = "EEWReciever";
 	public static final String NAME = "EEWReciever";
-	public static final String VERSION = "${version}";
-	public static final String FORGE = "${forgeversion}";
-	public static final String MINECRAFT = "${mcversion}";
+	public static final String VERSION = "2.4.7";
+	public static final String FORGE = "10.13.4.1558";
+	public static final String MINECRAFT = "1.7.10";
 
 	public static Logger logger = LogManager.getLogger(Reference.MODID);
 }
