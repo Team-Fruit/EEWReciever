18e8f3575eaafe2b2ba379908ce0dceab044a6f3
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-09-13 04:33:17 +0900

    Update 3.1.1

--- src/main/java/com/bebehp/mc/eewreciever/server/ServerAuthChecker.java
+++ src/main/java/com/bebehp/mc/eewreciever/server/ServerAuthChecker.java
@@ -5,7 +5,6 @@ import com.mojang.authlib.GameProfile;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ServerConfigurationManager;
 
@@ -16,16 +15,15 @@ public class ServerAuthChecker {
 
 	@SubscribeEvent
 	public void onPlayerLogin(final PlayerEvent.PlayerLoggedInEvent event) {
-		final GameProfile profile =  event.player.getGameProfile();
-		final ServerConfigurationManager configManager = MinecraftServer.getServer().getConfigurationManager();
-		final EntityPlayerMP playerMP = configManager.func_152612_a(profile.getName());
 		if (notification) {
+			final GameProfile profile =  event.player.getGameProfile();
+			final ServerConfigurationManager configManager = MinecraftServer.getServer().getConfigurationManager();
 			if (configManager.func_152596_g(profile)) {
-				playerMP.addChatComponentMessage(ChatUtil.byText("[EEWReciever]Twitter連携認証(緊急地震速報)がされていません！"));
-				playerMP.addChatComponentMessage(ChatUtil.byText("/eew setup でセットアップを開始します"));
-				playerMP.addChatComponentMessage(ChatUtil.byText("Twitter連携を無効にするには、configを変更し再起動して下さい"));
+				event.player.addChatComponentMessage(ChatUtil.byText("[EEWReciever]Twitter連携認証(緊急地震速報)がされていません！"));
+				event.player.addChatComponentMessage(ChatUtil.byText("/eew setup でセットアップを開始します"));
+				event.player.addChatComponentMessage(ChatUtil.byText("Twitter連携を無効にするには、configを変更し再起動して下さい"));
 			} else {
-				playerMP.addChatComponentMessage(ChatUtil.byText("[EEWReciever]Setupされていません。OPに連絡して下さい。"));
+				event.player.addChatComponentMessage(ChatUtil.byText("[EEWReciever]Setupされていません。OPに連絡して下さい。"));
 			}
 		}
 	}
