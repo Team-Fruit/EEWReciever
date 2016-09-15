f996b900724432d05eabf70128ff13f4ed994735
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-09-15 21:02:01 +0900

    Update 3.1.3

--- src/main/java/com/bebehp/mc/eewreciever/Reference.java
+++ src/main/java/com/bebehp/mc/eewreciever/Reference.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
 	public static final String MODID = "EEWReciever";
 	public static final String NAME = "EEWReciever";
-	public static final String VERSION = "3.1.2";
+	public static final String VERSION = "3.1.3";
 	public static final String FORGE = "10.13.4.1558";
 	public static final String MINECRAFT = "1.7.10";
 	public static final String PROXY_SERVER = "com.bebehp.mc.eewreciever.server.proxy.ServerProxy";
