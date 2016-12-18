7e6966eed97e1477d9413f650ff43695031d5df7
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-12-19 05:07:25 +0900

    debug

--- src/main/java/net/teamfruit/eewreciever2/Debug.java
+++ src/main/java/net/teamfruit/eewreciever2/Debug.java
@@ -1,10 +1,14 @@
 package net.teamfruit.eewreciever2;
 
+import net.teamfruit.eewreciever2.common.Reference;
+import net.teamfruit.eewreciever2.common.quake.observation.QuakeCalculator;
+import net.teamfruit.eewreciever2.common.quake.twitter.TweetQuakeNode;
+
 public class Debug {
 
 	public static void main(final String[] args) throws Exception {
-		//		final TweetQuakeNode node = new TweetQuakeNode().parseString("37,00,2016/11/22 06:00:33,0,8,ND20161122055958,2016/11/22 05:59:45,37.3,141.6,福島県沖,10,7.1,5強,1,1");
-		//		final double km = QuakeCalculator.angularDistance(node.lat, node.lon, 35.7067, 139.8683, 0);
-		//		final double km = QuakeCalculator.angularDistance(48.829322, 2.220172, 41.8905, 12.4926, 0);
+		final TweetQuakeNode node = new TweetQuakeNode().parseString("37,00,2016/11/22 06:00:33,0,8,ND20161122055958,2016/11/22 05:59:45,37.3,141.6,福島県沖,10,7.1,5強,1,1");
+		Reference.logger.info(QuakeCalculator.getMeasured(node.magnitude, node.depth, node.lat, node.lon, 36.9483f, 140.9033f, 1.7214609f));
+		//		Reference.logger.info(QuakeCalculator.getDistance(node.lat, node.lon, 36.9483, 140.9033, node.depth));
 	}
 }
--- src/main/java/net/teamfruit/eewreciever2/common/quake/observation/QuakeCalculator.java
+++ src/main/java/net/teamfruit/eewreciever2/common/quake/observation/QuakeCalculator.java
@@ -12,12 +12,25 @@ public class QuakeCalculator {
 	private QuakeCalculator() {
 	}
 
-	public static float getMeasured(final float magnitude, final float depth, final float epicenterLat, final float epicenterLon, final float pointLat, final float pointLon) {
+	/**
+	 * 計測震度を推定します
+	 * @param magnitude
+	 * @param depth
+	 * @param epicenterLat
+	 * @param epicenterLon
+	 * @param pointLat
+	 * @param pointLon
+	 * @param arv600
+	 * @return
+	 */
+	public static float getMeasured(final float magnitude, final float depth, final float epicenterLat, final float epicenterLon, final float pointLat, final float pointLon, final float arv600) {
 		final float mw = magnitude;
-		final double distance = angularDistance(epicenterLat, epicenterLon, pointLat, pointLon, depth);
+		final double distance = getDistance(epicenterLat, epicenterLon, pointLat, pointLon, depth);
 		final double fDistance = faultShortestDistance(distance, faultLength(mw));
-		final double pgv600 = pgv600(mw, depth, distance);
-		return 0;
+		final double pgv600 = pgv600(mw, depth, fDistance);
+		final double pgv = pgv(arv600, pgv600);
+		final float measured = (float) measured(pgv);
+		return measured;
 	}
 
 	/**
@@ -25,12 +38,9 @@ public class QuakeCalculator {
 	 * @param geographicalLatitude 地理緯度
 	 * @return 地心緯度
 	 */
-	@Deprecated
 	private static double toGeospatialLatitude(final double geographicalLatitude) {
-		return Math.atan(geographicalLatitude-Math.toRadians(11.55/60d)*Math.sin(2*geographicalLatitude));
+		return geographicalLatitude-Math.toRadians(11.55/60d)*Math.sin(2*geographicalLatitude);
 	}
-
-	public static final double A = 6378.137d;
 
 	/**
 	 * 震央距離を求めます
@@ -41,51 +51,32 @@ public class QuakeCalculator {
 	 * @param depth 震源深さ
 	 * @return 直線距離(Km)
 	 */
-	public static double angularDistance(double lat1, double lon1, double lat2, double lon2, final double depth) {
-		lat1 = Math.toRadians(lat1);
-		lat2 = Math.toRadians(lat2);
-		lon1 = Math.toRadians(lon1);
-		lon2 = Math.toRadians(lon2);
+	private static double getDistance(final double lat1, final double lon1, final double lat2, final double lon2, final double depth) {
+		return distance(toGeospatialLatitude(Math.toRadians(lat1)), Math.toRadians(lon1), toGeospatialLatitude(Math.toRadians(lat2)), Math.toRadians(lon2), depth);
+	}
+
+	private static final double A = 6370.291d;
+
+	/**
+	 * 震央距離を求めます
+	 * @param lat1 Radian(Geospatial)
+	 * @param lon1 Radian
+	 * @param lat2 Radian(Geospatial)
+	 * @param lon2 Radian
+	 * @param depth 震源深さ
+	 * @return 直線距離(Km)
+	 */
+	private static double distance(final double lat1, final double lon1, final double lat2, final double lon2, final double depth) {
 		final double x1 = Math.cos(lat1)*Math.cos(lon1)*((A-depth)/A);
 		final double y1 = Math.cos(lat1)*Math.sin(lon1)*((A-depth)/A);
 		final double z1 = Math.sin(lat1)*((A-depth)/A);
 		final double x2 = Math.cos(lat2)*Math.cos(lon2);
 		final double y2 = Math.cos(lat2)*Math.sin(lon2);
 		final double z2 = Math.sin(lat2);
-		final double a = Math.acos(x1*x2+y1*y2+z1*z2);
-		return Math.acos(x1*x2+y1*y2+z1*z2)*A;
-		//		final double ae = Math.cos(lat1)*Math.cos(lon1)*((A-depth)/A);
-		//		final double ax = Math.cos(lat2)*Math.cos(lon2);
-		//		final double be = Math.cos(lat1)*Math.sin(lon1)*((A-depth)/A);
-		//		final double bx = Math.cos(lat2)*Math.sin(lon2);
-		//		final double ce = Math.sin(lat1)*((A-depth)/A);
-		//		final double cx = Math.sin(lat2);
-		//		return Math.sqrt(Math.pow(ae-ax, 2)+Math.pow(be-bx, 2)+Math.pow(ce-cx, 2))*A;
+		//		final double a = Math.acos(x1*x2+y1*y2+z1*z2);
+		//		return Math.acos(x1*x2+y1*y2+z1*z2)*A;
+		return Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2)+Math.pow(z1-z2, 2))*A;
 	}
-
-	//ヒュベニの公式
-	/*	public static final double A = 6378137.000;
-		public static final double E2 = 0.00669438002301188;
-		public static final double MNUM = 6335439.32708317;
-
-		*//**
-			* 座標間の直線距離を求めます
-			* @param lat1
-			* @param lon1
-			* @param lat2
-			* @param lon2
-			* @return メートル
-			*//*
-			public static double getDistanceBetween(final double lat1, final double lon1, final double lat2, final double lon2) {
-			final double dx = Math.toRadians(lon1-lon2);
-			final double dy = Math.toRadians(lat1-lat2);
-			final double my = Math.toRadians((lat1+lat2)/2d);
-			final double w = Math.sqrt(1d-E2*Math.pow(Math.sin(my), 2));
-			final double n = A/w;
-			final double m = MNUM/Math.pow(w, 3);
-			return Math.sqrt(Math.pow(dy*m, 2)+Math.pow(dx*n*Math.cos(my), 2));
-			}
-			*/
 
 	/**
 	 * 断層最短距離を計算します
@@ -93,7 +84,7 @@ public class QuakeCalculator {
 	 * @param faultLength 断層長
 	 * @return 断層最短距離
 	 */
-	public static double faultShortestDistance(final double focusDistance, final double faultLength) {
+	private static double faultShortestDistance(final double focusDistance, final double faultLength) {
 		return Math.max(focusDistance-faultLength/2, 3.0d);
 	}
 
@@ -102,7 +93,7 @@ public class QuakeCalculator {
 	 * @param mw モーメントマグニチュード
 	 * @return 断層長
 	 */
-	public static double faultLength(final double mw) {
+	private static double faultLength(final double mw) {
 		return Math.pow(10, mw/2-1.85d);
 	}
 
@@ -111,7 +102,7 @@ public class QuakeCalculator {
 	 * @param mjma 気象庁マグニチュード
 	 * @return モーメントマグニチュード
 	 */
-	public static double momentMagnitude(final double mjma) {
+	private static double momentMagnitude(final double mjma) {
 		return mjma-0.171d;
 	}
 
@@ -122,26 +113,26 @@ public class QuakeCalculator {
 	 * @param faultShortestDistance 断層最短距離
 	 * @return S波の伝播速度が 600m/sに相当する硬質地盤上における地震動の最大速度振幅 (cm/s)
 	 */
-	public static double pgv600(final double mw, final double depth, final double faultShortestDistance) {
-		return Math.pow(10, mw*0.58d+0.0038d*depth-1.29d-Math.log10(faultShortestDistance+0.0028*Math.pow(10, mw*0.50d))-0.002*faultShortestDistance);
+	private static double pgv600(final double mw, final double depth, final double faultShortestDistance) {
+		return Math.pow(10, 0.58d*mw+0.0038d*depth-1.29d-Math.log10(faultShortestDistance+0.0028*Math.pow(10, mw*0.50d))-0.002*faultShortestDistance);
 	}
 
 	/**
 	 * 地表面での最大速度
-	 * @param arv600 地盤増幅度
+	 * @param arv 地盤増幅度
 	 * @param pgv600 S波の伝播速度が 600m /sに相当する硬質地盤上における地震動の最大速度振幅 (cm/s)
 	 * @return PGV
 	 */
-	public static double pgv(final double arv600, final double pgv600) {
-		return arv600*pgv600;
+	private static double pgv(final double arv, final double pgv600) {
+		return arv*pgv600;
 	}
 
 	/**
-	 * 震度4以上を対象とした計測震度
+	 * 震度4以上を対象とした計測震度<br>ここでは震度4以上を想定
 	 * @param pgv 地表面における最大速度 (cm/s)
 	 * @return 計測震度
 	 */
-	public static double measured(final double pgv) {
+	private static double measured(final double pgv) {
 		return 2.68d+1.72*Math.log10(pgv);
 	}
 }
