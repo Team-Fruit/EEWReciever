5879c642c67019b58a6265af74fab66da7d72201
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-12-23 23:28:47 +0900

    #

--- src/main/java/net/teamfruit/eewreciever2/Debug.java
+++ src/main/java/net/teamfruit/eewreciever2/Debug.java
@@ -3,25 +3,5 @@ package net.teamfruit.eewreciever2;
 public class Debug {
 
 	public static void main(final String[] args) throws Exception {
-		//		OvservationPredictor.INSTANCE.init();
-		//		new SeismicObservationPoints().get(new IJsonCallBack() {
-		//			@Override
-		//			public void onError(final Throwable t) {
-		//				Reference.logger.error(t.getMessage(), t);
-		//			}
-		//
-		//			@Override
-		//			public void onDone(final PointsJson json) {
-		//				for (final Entry<String, Map<String, Map<String, List<Point>>>> line1 : json.points.entrySet()) {
-		//					for (final Entry<String, Map<String, List<Point>>> line2 : line1.getValue().entrySet()) {
-		//						for (final Entry<String, List<Point>> line3 : line2.getValue().entrySet()) {
-		//							for (final Point line4 : line3.getValue()) {
-		//								Reference.logger.info("arv{}", line4.arv);
-		//							}
-		//						}
-		//					}
-		//				}
-		//			}
-		//		});
 	}
 }
